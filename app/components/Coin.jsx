01e0610ed871bc92cc9d9078a48047f2867f6eb8
mosespace <kisakyemoses50@gmail.com>
2023-07-06 09:50:25 +0300

    initial update

--- app/components/Coin.jsx
+++ app/components/Coin.jsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Coin = ({ data }) => {
-  const { uuid, iconUrl, name, symbol } = data;
+  const { uuid, iconUrl, name, symbol, tier } = data;
   return (
-    <Link href={`/coins/${uuid}`} className="coin">
+    <Link href={`/coins/${uuid}`} className="coinS">
       <Image src={iconUrl} alt={name} width={30} height={30} />
       <div className="details">
-        <h4>{name}</h4>
+        <h4>Name: {name}</h4>
         <h4>{symbol}</h4>
+        <p>Tier: {tier}</p>
       </div>
     </Link>
   );
